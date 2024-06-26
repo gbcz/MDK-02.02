3e23e13dce1f3af67bc7f997ad862870cea2341b
gbcz <59408093+gbcz@users.noreply.github.com>
2024-06-26 20:22:06 +1000

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+#Функция проверки на палиндром
 def is_palindrome(user_string):
     user_string = user_input.lower().replace(" ", "")
     return user_string == user_string[::-1]
 
-
+#Получение строки от пользователя
 user_input = input("Введите строку: ")
+#Вызов функции и вывод
 if is_palindrome(user_input):
     print(f'"{user_input}" является палиндромом')
 else:
     print(f'"{user_input}" не является палиндромом')
-    
